4438950e192f2a34678157558b125d4b855017cd
Tim <44300490+TimofiyJ@users.noreply.github.com>
2023-12-28 22:30:52 +0200

    feat: edge-based filter

--- ML_NovaPoshta_CV/app.py
+++ ML_NovaPoshta_CV/app.py
@@ -1,8 +1,13 @@
 import gradio as gr
+import os
 
 
 def define_class(image):
-
+    print(
+        os.system(
+            f'python ./models/lcnn/demo.py -d 0 ./models/lcnn/config/wireframe.yaml "C:/Users/Tymof/OneDrive/Desktop/education/Practice/Meduzzen_Intership/ML_NovaPoshta_CV/models/lcnn/190418-201834-f8934c6-lr4d10-312k.pth" "C:/Users/Tymof/OneDrive/Desktop/education/Practice/Meduzzen_Intership/ML_NovaPoshta_CV/data/images_data/1001(2023-06-07T08_06_44).jpg"  '
+        )
+    )
     return image
 
 
